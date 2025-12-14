d010f9eb17aa7d0079fe2698c5bd88cb7f7d7a81
nadav.beithalevi <nadav.beithalevi@gmail.com>
2025-12-14 09:59:05 +0200

    Refactor generate_parliamentary_script to include image generation and improve return type

--- app/the_parliement_service.py
+++ app/the_parliement_service.py
@@ -1,21 +1,38 @@
 from mcp.server.fastmcp import FastMCP
 from typing import Any
+from google import genai
 import logging
 
 mcp = FastMCP("the_parliament_service")
 
 @mcp.tool(name="the_parliament_service_tool", 
           description="Creates a parliamentary discussion on a given topic.")
-async def generate_parliamentary_script(script_topic: str) -> Any:
+async def generate_parliamentary_script(script_topic: str) -> dict[str, Any]:
     # Simulate fetching account info (replace with real logic)
+    client = genai.Client(api_key=os.getenv("GOOGLE_API_KEY")) 
+
+    prompt = (
+        "Create a picture of a nano banana dish in a fancy restaurant with a Gemini theme"
+    )
+
+    response = client.models.generate_content(
+        model="gemini-2.5-flash-image",
+        contents=[prompt],
+    )
+
+    
+    for part in response.parts:
+        if part.text is not None:
+            print(part.text)
+        elif part.inline_data is not None:
+            image = part.as_image()
+            return {    
+                "script_topic": "image created",
+                "english_script": f"Simulated parliamentary script on the topic: {image}"
+    }
+    
     logging.info(f"Generating parliamentary script for topic: {script_topic}")
     # return f"Generating parliamentary script for topic: some topic"
-    if not script_topic:
-        script_topic = "The impact of technology on society"
-    
-    # english_script = await parliament_agent_open_ai_sdk.run_parliament_session(script_topic) # type: ignore
-    return f"english_script: wheather"
-    
 
 @mcp.tool(name="hello_test_tool", 
           description="A simple tool that returns a greeting message.")
